07f3a4117b2043246b3b473d970ad50f5c99be74
trino2 <trino2@yahoo.com>
2018-02-11 05:10:56 +0000

    Page aligned to look good

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,39 @@ var express = require('express');
 const Instagram = require('node-instagram').default;
 var router = express.Router();
 
+// Create a new instance.
+const instagram = new Instagram({
+  clientId: process.env.clientID,
+  clientSecret: process.env.clientSecret,
+  accessToken: process.env.accessToken
+});
+
+// You can use callbacks or promises
+instagram.get('users/self/media/recent', (err, data) => {
+  if (err) {
+    console.log("nice try!");
+    console.log(err);
+  } else {
+    console.log('Your got this: ', data);
+  }
+});
+ 
+// Get information about the owner of the access_token.
+ var data = instagram.get('users/self');
+ console.log("User Data: ", data);
+ 
+// Handle errors
+instagram.get('tags/lol').then((data) => {
+  // console.log(data);
+}).catch((err) => {
+  // An error occured
+  console.log(err);
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-//  console.log('Data Length!!!!!!!!!!!!!!!!!!!!', data.length);
-  res.render('index', { title: 'TrinoTech'});
+  console.log('Data Lengt!!!!!!!!!!!!!!!!!!!!', data.length);
+  res.render('index', { title: 'TrinoTech', data: data});
 });
 
 module.exports = router;
